6461514b9261d4a90e5b7e532227c2f9e12d1145
bee <bernhard.spindler.75@gmail.com>
2019-12-12 00:05:33 +0100

    with perf

--- euler55/Program.cs
+++ euler55/Program.cs
@@ -11,16 +11,29 @@ namespace euler55
     {
         static void Main(string[] args)
         {
-            runLychrel(349);
-            runLychrel(10667);
-            runLychrel(10677);
-            runLychrel(196);
+            runWithProgress(196);
         }
-        static void runLychrel(int start)
+        static void runWithProgress(int start)
         {
+            Console.WriteLine($"calculating Lychrel for {start}");
             int iterations = 0;
+            Task calc = Task.Run(() => runLychrel(start, ref iterations));
+
+            int lastIter = 0;
+            int seconds = 0;
+            while ( !calc.Wait(1000) )
+            {
+                seconds++;
+                int currIter = iterations;
+                Console.WriteLine($"{seconds}s\t{currIter-lastIter}/s\t{currIter}");
+                lastIter = currIter;
+            }
+        }
+        static void runLychrel(int start, ref int iterations)
+        {
+            iterations = 0;
             var p = new PalinAdd[2] { new PalinAdd(start), new PalinAdd(0) };
-            Console.WriteLine($"(0)\t{p[0]}");
+            //Console.WriteLine($"(0)\t{p[0]}");
 
             int idx1 = 1;
             int idx2 = 0;
@@ -31,10 +44,9 @@ namespace euler55
 
                 p[idx1].selfPlusPalindrom(ref p[idx2]);
                 ++iterations;
-                
-                Console.WriteLine($"{iterations}\t{p[idx2]}");
-            } while (!p[idx2].isPalindrom());
 
+                //Console.WriteLine($"{iterations}\t{p[idx2]}");
+            } while (!p[idx2].isPalindrom());
         }
     }
 }
